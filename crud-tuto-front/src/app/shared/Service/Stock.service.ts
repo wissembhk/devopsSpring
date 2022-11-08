3d9746419ef2148d5286c699f6b0ccaf93fba9e4
wissembhk <58009610+wissembhk@users.noreply.github.com>
2022-11-08 15:13:10 +0200

    build

--- crud-tuto-front/src/app/shared/Service/Stock.service.ts
+++ crud-tuto-front/src/app/shared/Service/Stock.service.ts
@@ -7,7 +7,7 @@ import { HttpClient} from '@angular/common/http';
   providedIn: 'root'
 })
 export class StockService {
-  readonly API_URL = 'http://localhost:8089/SpringMVC/stock';
+  readonly API_URL = 'http://44.212.25.105:8089/SpringMVC/stock';
 
   constructor(private httpClient: HttpClient) { }
 
