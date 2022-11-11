1d445ba3857954d0a333fd4f34a59e1eb6d560d3
wissembhk <58009610+wissembhk@users.noreply.github.com>
2022-11-11 15:26:34 +0200

    test

---
+++ src/test/java/com/esprit/examen/services/SecteurActiviteImplTest.java
@@ -0,0 +1,60 @@
+package com.esprit.examen.services;
+
+import com.esprit.examen.entities.SecteurActivite;
+import com.esprit.examen.repositories.SecteurActiviteRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+class SecteurActiviteImplTest {
+    @Mock
+    SecteurActiviteRepository Repo;
+
+    @InjectMocks
+    SecteurActiviteServiceImpl Service;
+
+    SecteurActivite secteurActivite = SecteurActivite.builder().codeSecteurActivite("secteur").libelleSecteurActivite("lib").build();
+    List<SecteurActivite> listSecteurActivite = new ArrayList<SecteurActivite>() {
+        {
+            add(SecteurActivite.builder().codeSecteurActivite("secteur1").libelleSecteurActivite("lib1").build());
+            add(SecteurActivite.builder().codeSecteurActivite("secteur2").libelleSecteurActivite("lib2").build());
+        }
+    };
+
+    @Test
+    void testRetrieveSecteurActivite() {
+        Mockito.when(Repo.findById(Mockito.anyLong())).thenReturn(Optional.of(SecteurActivite));
+        @SuppressWarnings("removal")
+        SecteurActivite s1 = Service.retrieveSecteurActivite(new Long(2));
+        Assertions.assertNotNull(s1);
+    }
+
+    @Test
+    void testAllRetrievesecteurActivite() {
+        Mockito.when(Repo.findAll()).thenReturn(listSecteurActivites);
+        List<SecteurActivite> lSecteurActivite = Service.retrieveAllSecteurActivite();
+        Assertions.assertNotNull(lSecteurActivites);
+    }
+
+    @Test
+    void testAddsecteurActivite() {
+        Mockito.when(Repo.save(secteurActivite)).thenReturn(secteurActivite);
+        SecteurActivite s1 = Service.addStock(stock);
+        Assertions.assertNotNull(s1);
+
+    }
+
+
+
+}
