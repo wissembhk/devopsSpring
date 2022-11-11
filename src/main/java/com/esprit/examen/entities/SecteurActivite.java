f1da2c428cf9d721876cc508278fcf8046d2ec93
wissembhk <58009610+wissembhk@users.noreply.github.com>
2022-11-11 14:45:35 +0200

    add test

--- src/main/java/com/esprit/examen/entities/SecteurActivite.java
+++ src/main/java/com/esprit/examen/entities/SecteurActivite.java
@@ -12,10 +12,13 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.Builder;
+
 
 @Entity
 @Getter
 @Setter
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class SecteurActivite implements Serializable{
